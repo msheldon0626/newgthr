0b09dd860d81b5c83c95398b39f76c7a3b92df07
msheldon0626 <63196930+msheldon0626@users.noreply.github.com>
2020-04-06 18:31:46 -0500

    new

---
+++ app/web/js/models/firebase.js
@@ -0,0 +1,77 @@
+/* globals Firebase */
+
+!(function (exports) {
+  'use strict';
+
+  var archives = null;
+  var listeners = {};
+
+  var archiveHandler = {
+    archiveUpdates: function (evt) {
+      var handlers = listeners.value;
+      var archiveValues = Promise.resolve(evt.detail || {});
+      handlers && handlers.forEach(function (aHandler) {
+        archiveValues.then(aHandler.method.bind(aHandler.context));
+      });
+    }
+  };
+
+  function init() {
+    var self = this;
+    return new Promise(function (resolve) {
+      Utils.addEventsHandlers('roomController:', archiveHandler, exports);
+      resolve(self);
+    });
+  }
+
+  function addEventListener(type, aHandler) {
+    var context;
+    if (!(type in listeners)) {
+      listeners[type] = [];
+    }
+
+    var hd = aHandler;
+    if (typeof hd === 'object') {
+      context = hd;
+      hd = hd.handleEvent;
+    }
+
+    if (hd) {
+      listeners[type].push({
+        method: hd,
+        context: context
+      });
+    }
+  }
+
+  function removeEventListener(type, aHandler) {
+    if (!(type in listeners)) {
+      return false;
+    }
+    var handlers = listeners[type];
+    if (handlers) {
+      for (var i = 0, l = handlers.length; i < l; i++) {
+        var thisHandler = aHandler;
+        if (typeof thisHandler === 'object') {
+          thisHandler = aHandler.handleEvent;
+        }
+        if (handlers[i].method === thisHandler) {
+          handlers.splice(i, 1);
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  var FirebaseModel = {
+    addEventListener: addEventListener,
+    removeEventListener: removeEventListener,
+    init: init,
+    get archives() {
+      return archives;
+    }
+  };
+
+  exports.FirebaseModel = FirebaseModel;
+}(this));
